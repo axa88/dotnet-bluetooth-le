d9c1646328bab15edb19a738104bbe31fd3b5834
axa88 <axa88@yahoo.com>
2025-01-05 23:25:07 -0500

    merged latest WindowsBondStateRead... branch

--- Source/Plugin.BLE/Windows/Device.cs
+++ Source/Plugin.BLE/Windows/Device.cs
@@ -239,22 +239,11 @@ public class Device : DeviceBase<BluetoothLEDevice>
 		protected override DeviceBondState GetBondState()
 		{
 			if (NativeDevice == null)
-				Trace.Message($"Device was Disposed but application still has a reference, not good");
-			else
-			{
-				try
-				{
-					var deviceInformation = DeviceInformation.CreateFromIdAsync(NativeDevice.DeviceId).AsTask().Result;
+				NativeDevice = BluetoothLEDevice.FromBluetoothAddressAsync(Id.ToBleAddress()).AsTask().Result;
+
+			var deviceInformation = DeviceInformation.CreateFromIdAsync(NativeDevice.DeviceId).AsTask().Result;
 					return deviceInformation.Pairing.IsPaired ? DeviceBondState.Bonded : DeviceBondState.NotBonded;
 				}
-				catch (Exception ex)
-				{
-					Trace.Message($"GetBondState failed for {Name ?? Id.ToString()} : {ex.Message}");
-				}
-			}
-
-			return DeviceBondState.NotSupported;
-		}
 
 	public override bool UpdateConnectionParameters(ConnectParameters connectParameters = default) => MaybeRequestPreferredConnectionParameters(NativeDevice, connectParameters);
 }
